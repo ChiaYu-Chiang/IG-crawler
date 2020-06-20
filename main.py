4f64879b04e14fb147cb0eeedb5c43db57b29b9d
BrianChiang <brianick9@gmail.com>
2020-06-20 18:46:32 +0800

    修改main.py中cookie_data的註釋

--- main.py
+++ main.py
@@ -10,10 +10,11 @@ from pyquery import PyQuery as pq
 url_base = 'https://www.instagram.com/'
 uri_account = 'https://www.instagram.com/graphql/query/?query_hash=a5164aed103f24b03e7b7747a2d94e3c&variables=%7B%22id%22%3A%22{user_id}%22%2C%22first%22%3A12%2C%22after%22%3A%22{cursor}%22%7D'
 uri_hashtag = 'https://www.instagram.com/graphql/query/?query_hash=c769cb6c71b24c8a86590b22402fda50&variables=%7B%22tag_name%22%3A%22{user_id}%22%2C%22first%22%3A12%2C%22after%22%3A%22{cursor}%22%7D'
-# 透過cookie取得權限
+# 請更改cookie_data內的訊息，以成功獲取已追蹤非公開帳號的權限
 cookie_data = 'mid=XsvsoAALAAEYyrG6CF67BSwQ829S; fbm_124024574287414=base_domain=.instagram.com; fbsr_124024574287414=KukIXT55vhLdHf7sGB8Jwbp2v1E9Vkn9_I-54fr7rWc.eyJ1c2VyX2lkIjoiMTAwMDAwMzEyNDUyNDExIiwiY29kZSI6IkFRQ1ZweWFoRzVxcjFfR29DbnBXWGRhb0pXTFJ5YzNIdnlUa2xZR3FZbWVDcUFXZEdDeXRHY3pEZ3ZBQjJxbzJ6REc1c3VVZzdOMWh1ektVTzNfYnF1dnk5ZldUQTZpbDE3SEJ4ZU05X0RIRzBONjJ2eW5Tb2h2cjJ4dUVuSnlBbDRBQVRfeXNCdzJwQkVramJYZ1g5djdiamhCS1RZYllyOFZpOGhrUlpWcVVoZXBPVTdHRnVrUTd4WTQwY3RkVG5CZjhKUXV2LWVDVnhJSXl4Y1pfOVdDYnFTb1M1U2syb1p0NXFpZzI3amM1UjgzcUlQLTVzelg0SmdqbmlRRmwxbXNaYVVXUlNlVklxVWNoYXNjbmhPdkxnWGM4c0E0VmpUanNTUF9WSzhOZ2tyMlVVanViMHNmbldtU3hndnZBalhPWWZWMng2ci1yUTBXUTJvb2RCZC1oIiwib2F1dGhfdG9rZW4iOiJFQUFCd3pMaXhuallCQUZGa1AyY2NiQU1jR1pBSWEyUmp5cEE4M0FSUFZCWGFicGZaQUl4M2h1NlpBVU1ZbFRneVBNaVBmVUNMaWlMYW9USzJXR2k2dGhtQTVGc2NiTlBUSzBRV1BhZDZINDlqeTFjOFVvazJaQ2VCWkNrNGg5N0J2MWtLd1hLcHNiVFpBMTFZZ29JQU1RQUg4S1ZhWUl1Mkl2MGVSSjFCbEpBbjJtVGVZcGxYa24iLCJhbGdvcml0aG0iOiJITUFDLVNIQTI1NiIsImlzc3VlZF9hdCI6MTU5MDQyMzA0NX0; csrftoken=CAlYWJxx2K2Cpmvc9KWHmzN7dIoLuArs; ds_user_id=1637446255; sessionid=1637446255%3A9QqyGWXg8CetM9%3A17; shbid=3317; shbts=1591975326.7713547'
 headers = {
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36',
+    # 透過cookie取得權限
     'cookie': cookie_data
 }
 
